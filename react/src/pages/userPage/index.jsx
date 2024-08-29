39f0495df93e7971ba3640812ea46baa24cc4931
Madonna Adel <madonnadel0@gmail.com>
2024-08-29 17:08:49 +0300

    new update

--- react/src/pages/userPage/index.jsx
+++ react/src/pages/userPage/index.jsx
@@ -144,7 +144,7 @@ export default function UserPage() {
       </>
       ) : noResults && (
         <div className="mt-4">
-              <img src="no-result.svg" alt="" width='400px' />
+              <img src="no-result.svg mx-auto" alt="" width='400px' />
               <h5>لم يتم العثور على نتائج تطابق البحث.</h5>
         </div>
       )}
